74ba75c53a39dfc42f072fd1956b3c7e877dc55a
Daniel Muncaciu <danieldfgro2000@gmail.com>
2019-11-16 11:16:39 +0200

    first commit

--- thermometer.py
+++ thermometer.py
@@ -1,9 +1,9 @@
 class Thermometer():
-    def __init__(self, temp, celsius, fahrenheit):
+    def __init__(self, inside, temp, celsius, fahrenheit):
         self.temp = temp
         self.celsius = celsius
         self.fahrenheit = fahrenheit
-
+        self.inside = set.temp
 
     def get_temp(self, temp):
         self.inside = temp
@@ -14,10 +14,10 @@ class Thermometer():
 
     def to_celsius(self, temp):
         if "C" in temp:
-         self.temp = (temp * 9/5) + 32
+        self.temp = (temp * 9/5) + 32
         pass
 
-    def to_fahrenheit(self):
+    def to_fahrenheit(self, temp):
         if "F" in temp:
             self.temp = (temp - 32)*5/9
         pass
